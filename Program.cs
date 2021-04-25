d670392e8116e3e33cc9bb34fa39af4b6c0f98e1
Dylan Clement <dylanclement@gmail.com>
2021-04-25 10:38:18 +0800

    Save output to directory.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -39,7 +40,7 @@ namespace photo_organiser
                     var extension = Path.GetExtension(f).ToLower();
                     if (extension == ".jpg") {
                       AddImage(f);
-                    } if (extension == ".mp4") {
+                    } else if (extension == ".mp4") {
                       AddMovie(f);
                     } else {
                       Console.WriteLine($"Unknown extension {extension} for file {f}.");
@@ -48,6 +49,23 @@ namespace photo_organiser
                   catch (Exception ex)
                   {
                     Console.WriteLine($"Exception Occured: {ex.Message} for file {f}.");
+                  }
+                }
+            }
+            public void SaveOutput(string outputDir)
+            {
+                foreach(var f in fileList)
+                {
+                  var year = f.Key.Substring(0,4);
+
+                  var destDir = Path.Join(outputDir, Path.Join(year, f.Key));
+                  if (!Directory.Exists(destDir))
+                  {
+                    Directory.CreateDirectory(destDir);
+                  }
+                  foreach (var fileName in f.Value)
+                  {
+                    File.Copy(fileName, Path.Join(destDir, Path.GetFileName(fileName)));
                   }
                 }
             }
@@ -63,9 +81,56 @@ namespace photo_organiser
                 fileList[dateKey].Add(f);
             }
 
-            private void AddMovie(string f)
+            private void AddMovie(string fileName)
             {
-                throw new NotImplementedException();
+              var strDateIndex = fileName.IndexOf("20");
+              if (strDateIndex == -1) 
+              {
+                Console.WriteLine($"Unable to find a date in fileName {fileName}.");
+                return;
+              }
+
+              var dateKey = $"{fileName.Substring(strDateIndex, 4)}-{fileName.Substring(strDateIndex, 2)}-{fileName.Substring(strDateIndex, 2)}";
+              Console.WriteLine($"File {fileName} was taken on {dateKey}");
+
+              // Attempt to shrink movie using ffmpeg, and use new filename if it is smaller
+              // Create a temp file with same name in $TMP folder
+              var destFile = Path.GetTempPath() + Path.GetFileName(fileName);
+              // Delete file if already exists
+              File.Delete(destFile);
+              // Create new temp file
+
+              // Run ffmpeg to try and shrink the file
+              var procInfo = new ProcessStartInfo("ffmpeg", $"-i {fileName} -c:v libx264 -preset medium -crf 25 -movflags +faststart -acodec aac -strict experimental -ab 96k {destFile}");
+              try
+              {
+                var procResult = Process.Start(procInfo);
+                procResult.WaitForExit();
+              }
+              catch (Exception ex)
+              {
+                Console.WriteLine($"Exception Occured: {ex.Message} for file {fileName}.");
+              }
+
+              // See if new file is indeed smaller
+              var fileSize = new FileInfo(fileName).Length; 
+              var newFileSize = new FileInfo(destFile).Length; 
+              
+              var ratio = newFileSize / (float)fileSize;
+              if (ratio < 0.93)
+              {
+		            var newRatio = (1 - ratio) * 100;
+		            Console.WriteLine($"Using shrunk movie file {newRatio}% reduction).");
+
+                // Use new file as 
+                fileName = destFile;
+              }
+
+              // Get date string from filename
+              if (!fileList.ContainsKey(dateKey)) {
+                fileList[dateKey] = new List<string>();
+              }
+              fileList[dateKey].Add(fileName);
             }
 
             //we init this once so that if the function is repeatedly called
@@ -90,7 +155,13 @@ namespace photo_organiser
 
             Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
             {
-              fadService.ProcessDir(o.InputDir, o.OutputDir);
+              if (!Directory.Exists(o.InputDir) || !Directory.Exists(o.OutputDir))
+              {
+                Console.WriteLine($"Make sure directories exist.");
+                return;
+              }
+              fadService.ProcessDir(o.InputDir, o.OutputDir);              
+              fadService.SaveOutput(o.OutputDir);
             });
         }
     }
