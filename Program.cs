5766736e9a991324dae7c380c26e41f304c8a5d4
Dylan Clement <dylanclement@gmail.com>
2022-08-24 14:22:33 +0800

    Fix issue with date detection in moviews. Using h265 as default.

--- Program.cs
+++ Program.cs
@@ -95,74 +95,83 @@ namespace photo_organiser
 
             private void AddMovie(string fullPath)
             {
-              var fileName = Path.GetFileName(fullPath);
-              var strDateIndex = fileName.LastIndexOf("20");
-              var dateKey = "";
-              
-              // If the string contains a 20 and it's at least 8 characters long
-              if (strDateIndex != -1 && fileName.Length - strDateIndex > 8) 
-              {
-                dateKey = $"{fileName.Substring(strDateIndex, 4)}-{fileName.Substring(strDateIndex + 4, 2)}-{fileName.Substring(strDateIndex + 6, 2)}";                
-              } else {
-                dateKey = File.GetLastWriteTime(fullPath).ToString("yyyy-MM-dd");
-              }           
-              
-              Console.WriteLine($"File {fullPath} was taken on {dateKey}");
+                var fileName = Path.GetFileName(fullPath);
 
-              // Attempt to shrink movie using ffmpeg, and use new filename if it is smaller
-              // Create a temp file with same name in $TMP folder
-              var destFile = Path.GetTempPath() + fileName;
-              // Delete file if already exists
-              File.Delete(destFile);
+                var regex = new Regex(@"(\d{4})(\d{2})(\d{2})");
+                var dateKey = "";
 
-              // Run ffmpeg to try and shrink the file
-              // libx264 = CPU 
-              // H264_AMF = GPU
+                var match = regex.Match(fileName);
+                // If the string contains a 20 and it's at least 8 characters long
+                if (match.Success) 
+                {
+                    dateKey = $"{match.Groups[1]}-{match.Groups[2]}-{match.Groups[3]}";
+                } else {
+                    dateKey = File.GetLastWriteTime(fullPath).ToString("yyyy-MM-dd");
+                }           
               
-              // 264
-              //var cmdLineArgs = $"-i \"{fullPath}\" -c:v h264_amf -preset medium -crf 25 -movflags +faststart -acodec aac -strict experimental -ab 96k \"{destFile}\"";
+                Console.WriteLine($"File {fullPath} was taken on {dateKey}");
 
-              // 265
-              //var cmdLineArgs = $"-i \"{fullPath}\" -c:v libx265 -preset medium -crf 25 -movflags +faststart -acodec aac -strict experimental -ab 96k \"{destFile}\"";
-              var cmdLineArgs = $"-i \"{fullPath}\" -c:v hevc_amf -rc cqp -qp_i 26 -qp_p 36 -c:a copy -movflags +faststart  \"{destFile}\"";
-              Console.WriteLine($"Executing ffmpeg {cmdLineArgs}");
+                // Attempt to shrink movie using ffmpeg, and use new filename if it is smaller
+                // Create a temp file with same name in $TMP folder
+                var destFile = Path.GetTempPath() + fileName;
+                // Delete file if already exists
+                File.Delete(destFile);
+
+                // Run ffmpeg to try and shrink the file
+                // libx264 = CPU 
+                // H264_AMF = GPU
+
+                // 264
+                //var cmdLineArgs = $"-i \"{fullPath}\" -c:v h264_amf -preset medium -crf 25 -movflags +faststart -acodec aac -strict experimental -ab 96k \"{destFile}\"";
+
+                // 265
+                var cmdLineArgs = $"-i \"{fullPath}\" -c:v libx265 -preset medium -crf 25 -movflags +faststart -acodec aac -strict experimental -ab 96k \"{destFile}\"";
+
+                // Used RX 6600 XT for hardware encoding h264
+                //var cmdLineArgs = $"-i \"{fullPath}\" -c:v hevc_amf -rc cqp -qp_i 10 -qp_p 15 -c:a copy -movflags +faststart  \"{destFile}\"";
+
+                // Testing hw h265              
+                //r cmdLineArgs = $"-i \"{fullPath}\" -vaapi_device /dev/dri/renderD128 -i 'concat:00001.ts|00002.ts|00003.ts' -vf 'format=nv12,hwupload' -map 0:0  -c:v hevc_vaapi -map 0:a -c:a copy -rc_mode CQP -global_quality 25 -profile:v main -v verbose \"{destFile}\"";
+
+
+                Console.WriteLine($"Executing ffmpeg {cmdLineArgs}");
               
-              // Set process info
-              var procInfo = new ProcessStartInfo("ffmpeg", cmdLineArgs);
-              procInfo.RedirectStandardOutput = true;
-              procInfo.UseShellExecute = false;
+                // Set process info
+                var procInfo = new ProcessStartInfo("ffmpeg", cmdLineArgs);
+                procInfo.RedirectStandardOutput = true;
+                procInfo.UseShellExecute = false;
 
-              // wrap in exception trying to execute external process
-              try
-              {
+                // wrap in exception trying to execute external process
+                try
+                {
                 var procResult = Process.Start(procInfo);
                 procResult.WaitForExit();
-              }
-              catch (Exception ex)
-              {
+                }
+                catch (Exception ex)
+                {
                 Console.WriteLine($"Exception Occured: {ex.Message} for file {fullPath}.");
-              }
+                }
 
-              // See if new file is indeed smaller
-              var fileSize = new FileInfo(fullPath).Length; 
-              var newFileSize = new FileInfo(destFile).Length; 
+                // See if new file is indeed smaller
+                var fileSize = new FileInfo(fullPath).Length; 
+                var newFileSize = new FileInfo(destFile).Length; 
               
-              // Check if the newly produced file is smallere than the
-              var ratio = newFileSize / (float)fileSize;
-              if (ratio < 0.93)
-              {
+                // Check if the newly produced file is smallere than the
+                var ratio = newFileSize / (float)fileSize;
+                if (ratio < 0.93)
+                {
 		            var newRatio = (1 - ratio) * 100;
 		            Console.WriteLine($"Using shrunk movie file {newRatio}% reduction).");
 
                 // Use new file as 
                 fullPath = destFile;
-              }
+                }
 
-              // Get date string from filename
-              if (!fileList.ContainsKey(dateKey)) {
+                // Get date string from filename
+                if (!fileList.ContainsKey(dateKey)) {
                 fileList[dateKey] = new List<string>();
-              }
-              fileList[dateKey].Add(fullPath);
+                }
+                fileList[dateKey].Add(fullPath);
             }
 
             //we init this once so that if the function is repeatedly called
@@ -190,8 +199,7 @@ namespace photo_organiser
         static void Main(string[] args)
         {
             var fadService = new FileAndDirectoryService();
-            
-
+           
             Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
             {
               if (!Directory.Exists(o.InputDir) || !Directory.Exists(o.OutputDir))
